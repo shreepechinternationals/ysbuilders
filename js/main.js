e05ce148d408698e688c8e2ae8e72018949ef4f5
shreepechinternationals <shreepechinternationals@gmail.com>
2024-09-16 14:10:30 +0545

    animation-2

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@
     
     // Back to top button
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
@@ -40,6 +40,7 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
+
 
 
     // Portfolio isotope and filter
@@ -63,6 +64,9 @@
         dots: false,
         loop: true,
     });
+
+    // Initiate the wowjs
+    new WOW().init();
     
 })(jQuery);
 
